52efee5eaf00295f6e70c0d13a9a0958933afe76
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-22 12:11:39 -0700

    created setUpInput to listen for user input

--- play.js
+++ play.js
@@ -1,3 +1,19 @@
 const connect = require(`./client`);
-
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  const handleUserInput = (input) => {
+    if (input === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
