39f9156e3f546679f140725bd8bbda40077f6c97
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-22 14:47:18 -0700

    created to handle constant values

---
+++ constants.js
@@ -0,0 +1,18 @@
+const IP = 'localhost';
+const PORT = 50541;
+
+const moveUp = "w";
+const moveLeft = "a";
+const moveDown = "s";
+const moveRight = "d";
+
+
+
+module.exports = {
+  IP,
+  PORT,
+  moveUp,
+  moveLeft,
+  moveDown,
+  moveRight
+};
